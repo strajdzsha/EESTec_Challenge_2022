cb3cc4cea3229a4b6429cb2c5a873d2851d733ae
strajdzsha <superstrale2002@gmail.com>
2022-03-19 17:04:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from sklearn import preprocessing
 from sklearn import metrics
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn import tree
-from sklearn.datasets import load_iris
-import pickle
+from imblearn.over_sampling import RandomOverSampler
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -27,6 +26,13 @@ def show_conf_matrix(y_test, y_pred, classes):
     plt.ylabel('True value')
 
     plt.show()
+
+
+def plotDecisionTree():
+    fig = plt.figure(figsize=(100, 100))
+    _ = tree.plot_tree(clf, max_depth=3, feature_names=['X', 'Y', 'MD', 'GR', 'RT', 'DEN', 'CN', 'D_Env'],
+                       filled=True)
+    fig.savefig("decision_tree.png")
 
 
 df = pd.read_csv('Train-dataset.csv')
@@ -47,29 +53,23 @@ Feature = df[['X', 'Y', 'MD', 'GR', 'RT', 'DEN', 'CN', 'D_Env']]
 X = Feature
 X= preprocessing.StandardScaler().fit(X).transform(X)
 Y = df['LITH_CODE']
+print(Y.value_counts())
+
+oversample = RandomOverSampler(sampling_strategy='not majority')
+X, Y = oversample.fit_resample(X, Y)
+print(Y.value_counts())
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=7)
 
 print ('Train set:', X_train.shape,  y_train.shape)
 print ('Test set:', X_test.shape,  y_test.shape)
 
-clf = tree.DecisionTreeClassifier(criterion="entropy")
+clf = tree.DecisionTreeClassifier(criterion="entropy", max_depth=15)
 clf = clf.fit(X_train, y_train)
 
-#fig = plt.figure(figsize=(100, 100))
-#_ = tree.plot_tree(clf, max_depth=3, feature_names=['X', 'Y', 'MD', 'GR', 'RT', 'DEN', 'CN', 'D_Env'],
-#                    filled=True)
-#fig.savefig("decision_tree.png")
 
-# predTree = clf.predict(X_test)
-# print("DecisionTrees's Accuracy: ", metrics.accuracy_score(y_test, predTree))
-
-y_predict = clf.predict(X_test)
-
-print("Train set accuracy: ", round(metrics.f1_score(y_train, clf.predict(X_train), average = 'micro'), 2))
-print("Test set accuracy: ", round(metrics.f1_score(y_test, clf.predict(X_test), average = 'micro'), 2))
-
-
+print("Train set accuracy: ", round(metrics.f1_score(y_train, clf.predict(X_train), average = 'micro'), 5))
+print("Test set accuracy: ", round(metrics.f1_score(y_test, clf.predict(X_test), average = 'micro'), 5))
 # PLOTTING CONFUSION MATRIX
 # target_lithologys = []
 # labels = np.sort(y_test.unique())
