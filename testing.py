4a723faa27687dff11ac2a08dbe004ba53519fc7
strajdzsha <superstrale2002@gmail.com>
2022-03-21 10:26:26 +0100

    Last version of code

--- testing.py
+++ testing.py
@@ -48,8 +48,29 @@ def addClusters(df):
     return df
 
 
-clf = load('model_new.joblib')
-scaler = load('scaler_new.joblib')
+def AddClusterRock(df):
+    k_means_rock = load('k_means_rock.joblib')
+
+    Feature = df[['MD', 'GR', 'DEN', 'CN']]
+    X = Feature.values
+
+    scaler = preprocessing.MaxAbsScaler()
+    X = scaler.fit_transform(X)
+
+    k_means_labels = k_means_rock.predict(X)
+    labels = pd.DataFrame(k_means_labels)
+
+    clusterNum = 15
+    df['Cluster'] = labels
+
+    MAPPING = {0: 772.0997123681688, 1: 402.3287300297861, 2: 649.7727272727273, 3: 731.9670631290028, 4: 602.8231797919763, 5: 468.848167539267, 6: 550.2277432712216, 7: 393.0073126142596, 8: 680.8269152817187, 9: 662.7157652474108, 10: 298.77675840978594, 11: 597.19563765858, 12: 686.3690577611177, 13: 742.1259842519685, 14: 641.016548463357}
+    df['Cluster'] = df['Cluster'].apply(lambda x: MAPPING[x])
+
+    return df
+
+
+clf = load('model_cluster.joblib') # ovaj je 0.87
+scaler = load('scaler_cluster.joblib')
 test_df = pd.read_csv('Test-dataset.csv')
 
 MAPPING = {
@@ -61,6 +82,7 @@ MAPPING = {
 test_df['D_Env'] = test_df['DEPOSITIONAL_ENVIRONMENT'].apply(lambda x: MAPPING[x])
 
 test_df = addClusters(test_df)
+test_df = AddClusterRock(test_df)
 
 MAPPING = {
     0: 630,
@@ -69,7 +91,9 @@ MAPPING = {
 }
 test_df['Pos'] = test_df['POSITION_CLUSTER'].apply(lambda x: MAPPING[x])
 
-feature_list = ['Pos', 'MD', 'GR', 'RT', 'DEN', 'CN', 'D_Env']
+print(test_df.head(10))
+
+feature_list = ['Pos', 'MD', 'GR', 'RT', 'DEN', 'CN', 'D_Env', 'Cluster']
 
 X = test_df[feature_list]
 X = scaler.transform(X)
